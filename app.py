accd35b8244bee696b8bec759a5b23b71a470ab6
Grant Stenger <gstenger98@gmail.com>
2020-11-28 02:04:14 -0600

    content update

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ def essays():
 def papers():
     return render_template('papers.html')
 
-@app.route('/gifts')
-def gifts():
-    return render_template('gifts.html')
+@app.route('/gift-ideas')
+def gift_ideas():
+    return render_template('gift-ideas.html')
 
 if __name__ == "__main__":
     app.run(debug=True)

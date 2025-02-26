1d1a17e8d158017bf746fe14eca2c4a658e6d9ee
Grant Stenger <grant@kinetic.xyz>
2025-02-25 19:54:34 -0800

    pdf handling

--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { articles } from '../../../data/articles'
 import ArticlePage from '../../../components/ArticlePage'
+import { redirect } from 'next/navigation'
 
 export async function generateStaticParams() {
   return articles.map((article) => ({
@@ -12,6 +13,12 @@ export default function Article({ params }: { params: { slug: string } }) {
 
   if (!article) {
     return <div>Article not found</div>
+  }
+
+  // If the article has a pdfUrl, redirect to it
+  if (article.pdfUrl) {
+    // Use server-side redirect for PDFs
+    return redirect(article.pdfUrl);
   }
 
   return (

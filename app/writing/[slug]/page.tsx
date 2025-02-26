c192c47eee7e8b2c24849d8ff58bfad7fbfe4bf7
Grant Stenger <grant@kinetic.xyz>
2025-02-25 21:01:12 -0800

    fix

--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -1,24 +1,26 @@
 import { articles } from '../../../data/articles'
 import ArticlePage from '../../../components/ArticlePage'
-import { redirect } from 'next/navigation'
+import { notFound } from 'next/navigation'
 
 export async function generateStaticParams() {
-  return articles.map((article) => ({
-    slug: article.slug,
-  }))
+  // Only generate params for non-PDF articles
+  return articles
+    .filter(article => !article.pdfUrl)
+    .map((article) => ({
+      slug: article.slug,
+    }))
 }
 
 export default function Article({ params }: { params: { slug: string } }) {
   const article = articles.find((a) => a.slug === params.slug)
 
   if (!article) {
-    return <div>Article not found</div>
+    return notFound()
   }
 
-  // If the article has a pdfUrl, redirect to it
+  // Don't try to handle PDF articles in this route at all
   if (article.pdfUrl) {
-    // Use server-side redirect for PDFs
-    return redirect(article.pdfUrl);
+    return notFound()
   }
 
   return (

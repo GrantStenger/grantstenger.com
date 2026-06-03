8c598f670f9dd065e44bad9bc54625b0d74a5013
Grant Stenger <gstenger98@gmail.com>
2026-06-03 12:00:04 -0400

    Remove Tweets from nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,7 +33,6 @@ export function Header({ className = '' }: HeaderProps) {
                         <Link href="/books" className="text-gray-400 hover:text-white transition-colors duration-200 underline text-sm md:text-base">BOOKS</Link>
                         <Link href="/essays" className="text-gray-400 hover:text-white transition-colors duration-200 underline text-sm md:text-base">ESSAYS</Link>
                         <Link href="/quotes" className="text-gray-400 hover:text-white transition-colors duration-200 underline text-sm md:text-base">QUOTES</Link>
-                        <Link href="/tweets" className="text-gray-400 hover:text-white transition-colors duration-200 underline text-sm md:text-base">TWEETS</Link>
                     </div>
                     <div className="flex space-x-2 md:space-x-4">
                         <SocialLink href="https://twitter.com/GrantStenger" icon={<TwitterIcon />} label="Twitter" />
